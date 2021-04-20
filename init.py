fe77a2f92b1420a3208a21672920a46885933654
WillDunkley <willj.dunkley@gmail.com>
2021-04-20 13:24:50 +0100

    innitisation for JSON file

--- init.py
+++ init.py
@@ -1,4 +1,6 @@
 #INITAL SETUP OF JSON FILE
+
+#The dictionary setup
 data = {}
 data['tally'] = []
 data['battery'] = []
@@ -14,12 +16,14 @@ data['battery'].append({
     'percentIncr': 0,
     'percentDecr': 0,
 })
+
 data['diagRequest'].append({
     'camera': 1,
     'imagStor': 0,
     'other': 0,
 })
 
+#Sending the dictionary to the json file
 with open('dataflow.json', 'w') as jsonFile:
     json.dump(data, jsonFile)
 
