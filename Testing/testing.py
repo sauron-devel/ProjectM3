e45da4e4a2a41b871f38553ebe6e42fae158760e
WillDunkley <willj.dunkley@gmail.com>
2021-04-20 16:34:36 +0100

    Testing successfully logs errors

--- Testing/testing.py
+++ Testing/testing.py
@@ -1,43 +1,83 @@
 import json
 import time
+import logging
 
-#INITAL SETUP OF JSON FILE
-data={}
-data['tally'] = []
-data['battery'] = []
-data['diagRequest'] = []
+data = [0 for i in range(8)]
+errorFound = False
+logging.basicConfig(filename = 'log.txt', filemode = 'a', format = '%(asctime)s - %(levelname)s - %(message)s')
 
-data['tally'].append({
-    'incr': 0,
-    'decr': 0,
-})
-data['battery'].append({
-    'powerFlag': 1,
-    'percentIncr': 0,
-    'percentDecr': 0,
-})
-data['diagRequest'].append({
-    'camera': 1,
-    'imagStor': 0,
-    'other': 0,
-})
+def errorCreate(val, place, errorCode):
+    place = placeOfError(place)
+    if errorCode == 1:
+        logging.error("Invalid '%s' in %s value - Error with %s", val, place, str(type(val)))
+        return
+    if errorCode == 2:
+        logging.error("Invalid Boolean Expression at the %s value", place)
+        return
 
-with open('dataflow.json', 'w') as jsonFile:
-    json.dump(data, jsonFile)
+def placeOfError(place):
+    if place == 0:
+        return 'Increment Tally'
+    elif place == 1:
+        return 'Decrement Tally'
+    elif place == 2:
+        return 'Power Flag'
+    elif place == 3:
+        return 'Increment Power Percentage'
+    elif place == 4:
+        return 'Decrement Power Percentage'
+    elif place == 5:
+        return 'Camera Request'
+    elif place == 6:
+        return 'Image Storage Request'
+    elif place == 7:
+        return 'Other Request'
 
-jsonFile.close()
 
 #CONTINOUS READING OF JSON FILE AND TESTING
 while 1:
-    jsonFile = open('dataflow.json')
-    data = json.load(jsonFile)
+    jsonFile1 = open('output.json')
+    jsonFile2 = open('config.json')
+    jsonFile3 = open('input.json')
+    rawData1 = json.load(jsonFile1)
+    rawData2 = json.load(jsonFile2) 
+    rawData3 = json.load(jsonFile3)
+    errorFound = False
+    #Loading data into variable
 
-    for i in data['tally']:
-        tempIncr = i['incr']
-        tempDecr = i['decr']
+    for i in rawData1['tally']:
+        data[0] = i['incr']
+        data[1] = i['decr']
 
-    test = isinstance(tempIncr, str)
-    print('String? ' + str(test))
+    for i in rawData1['battery']:
+        data[2] = i['powerFlag']
+        data[3] = i['percentIncr']
+        data[4] = i['percentDecr']
 
-    jsonFile.close()
-    time.sleep(0.1)
+    for i in rawData1['diagRequest']:
+        data[5] = i['camera']
+        data[6] = i['imagStor']
+        data[7] = i['other']
+
+    #Testing
+    listIncr = 0
+    for i in data: 
+        if not type(i) == int:
+            errorCreate(i, listIncr, 1)
+            errorFound = True
+
+        if not ((i == 1) or (i == 0)):
+            errorCreate(i, listIncr, 2)
+            errorFound = True
+        listIncr += 1
+
+    jsonFile1.close()
+    jsonFile2.close()
+    jsonFile3.close()
+    time.sleep(0.5) 
+
+    #Refresh and clean the log file
+    if errorFound == True:
+        with open("log.txt", "w") as logFile:
+            logFile.truncate(0)
+
